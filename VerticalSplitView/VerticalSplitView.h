e25c454650086966193e5cc8b0fb7bd3db15a3f4
AKIRA-MIYAKE <akira.aktk.miyake@gmail.com>
2015-10-18 22:33:36 +0900

    Initial Commit

---
+++ VerticalSplitView/VerticalSplitView.h
@@ -0,0 +1,19 @@
+//
+//  VerticalSplitView.h
+//  VerticalSplitView
+//
+//  Created by MiyakeAkira on 2015/10/18.
+//  Copyright © 2015年 MiyakeAkira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VerticalSplitView.
+FOUNDATION_EXPORT double VerticalSplitViewVersionNumber;
+
+//! Project version string for VerticalSplitView.
+FOUNDATION_EXPORT const unsigned char VerticalSplitViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerticalSplitView/PublicHeader.h>
+
+
